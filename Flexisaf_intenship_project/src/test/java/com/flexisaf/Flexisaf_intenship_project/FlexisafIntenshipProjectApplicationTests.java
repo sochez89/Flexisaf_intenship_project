54571127269403fbb6077ded3b9d620c977a5f69
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-05-15 11:36:36 +0100

    Initial commit

---
+++ Flexisaf_intenship_project/src/test/java/com/flexisaf/Flexisaf_intenship_project/FlexisafIntenshipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexisaf.Flexisaf_intenship_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexisafIntenshipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
