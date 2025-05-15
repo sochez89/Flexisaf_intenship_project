dc7abf4273bb4e53c018619eebf02e4c2553e9d2
SOCHEZ89 <50618554+sochez89@users.noreply.github.com>
2025-05-15 13:04:46 +0100

    Add files via upload

---
+++ Flexisaf_intenship_project/src/main/java/com/flexisaf/Flexisaf_intenship_project/FlexisafIntenshipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flexisaf.Flexisaf_intenship_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexisafIntenshipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexisafIntenshipProjectApplication.class, args);
+	}
+
+}
