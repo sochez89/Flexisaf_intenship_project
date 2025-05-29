0c0b0c7cabc8aef62e29583c9b2c01c662386db4
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-05-29 09:13:55 +0100

    initial commit

---
+++ framework_architecture/src/main/java/com/Framework_architecture/framework_architecture/FrameworkArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.Framework_architecture.framework_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkArchitectureApplication.class, args);
+	}
+
+}
---
+++ framework_architecture/src/test/java/com/Framework_architecture/framework_architecture/FrameworkArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Framework_architecture.framework_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
