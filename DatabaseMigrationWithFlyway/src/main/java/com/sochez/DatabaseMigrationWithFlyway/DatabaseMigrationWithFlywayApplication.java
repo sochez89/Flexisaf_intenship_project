b2facf0979b0b1af4fda3940d981bbe97476d81f
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-06-11 12:08:07 +0100

    week 5 project skeleton added

---
+++ DatabaseMigrationWithFlyway/src/main/java/com/sochez/DatabaseMigrationWithFlyway/DatabaseMigrationWithFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.sochez.DatabaseMigrationWithFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationWithFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMigrationWithFlywayApplication.class, args);
+	}
+
+}
---
+++ DatabaseMigrationWithFlyway/src/test/java/com/sochez/DatabaseMigrationWithFlyway/DatabaseMigrationWithFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochez.DatabaseMigrationWithFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMigrationWithFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
