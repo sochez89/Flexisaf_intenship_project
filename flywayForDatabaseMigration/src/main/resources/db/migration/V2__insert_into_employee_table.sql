d7df90f878cde51f6a9d70317ff5d5d4dc48534a
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-06-18 13:22:16 +0100

    added V2... migration file to insert data to the employee table

---
+++ flywayForDatabaseMigration/src/main/resources/db/migration/V2__insert_into_employee_table.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO employee (firstName, lastName, email,sex,phoneNumber,
+                        NOKFirstName,NOKLastName, NOKEmail,NOKPhoneNumber)
+VALUES
+('Sochez','Eze', 'sochez@gmail.com', 'male', '08030718231', 'Rose', 'Onah', 'Rose1@gmail.com', '08077739667'),
+('Solomon','Ezeatum', 'solomon@gmail.com', 'male', '08030718230', 'Rosemary', 'Ezeatum', 'Rosy@gmail.com', 08077739666),
+('Franca','Eze', 'frank@gmail.com', 'female', '08030718233', 'Helen', 'Onah', 'helen@gmail.com', '08077739668')
