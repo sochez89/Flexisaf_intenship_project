b635e70b6b5383d1a5da4fd8a2a5b051803dc90e
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-07-03 11:33:28 +0100

    week eight project structure added

---
+++ RESTful_API_best_practices/src/main/java/com/sochez/RESTful_API_best_practices/ResTfulApiBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.sochez.RESTful_API_best_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiBestPracticesApplication.class, args);
+	}
+
+}
---
+++ RESTful_API_best_practices/src/test/java/com/sochez/RESTful_API_best_practices/ResTfulApiBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochez.RESTful_API_best_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiBestPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
