dd770cf866e8e4d822ba86649375f69833e4bc72
SolomonNewEra <ezeatumsolomon@gmail.com>
2025-07-16 22:15:10 +0100

    second week of week 9 project template added

---
+++ TestingAndMocking/src/main/java/com/sochez/TestingAndMocking/TestingAndMockingApplication.java
@@ -0,0 +1,13 @@
+package com.sochez.TestingAndMocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAndMockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingAndMockingApplication.class, args);
+	}
+
+}
---
+++ TestingAndMocking/src/test/java/com/sochez/TestingAndMocking/TestingAndMockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochez.TestingAndMocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAndMockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
